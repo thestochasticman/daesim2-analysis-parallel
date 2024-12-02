d43a692046b2118954249e347247046d1115387d
Alexander Norton <alexander.norton@climate-energy-college.org>
2024-12-02 12:08:05 +1100

    Initial commit: Add DAESIM2 analysis package

--- README.md
+++ README.md
@@ -1,2 +1,156 @@
-# daesim2-analysis
+# DAESIM2 Analysis
+
+## Description
+
 This Python package supports the development and testing of the Dynamic Agro-Ecosystem Simulator 2 (DAESIM2), a biophysical model for agro-ecosystems. The package provides tools for conducting sensitivity analysis, calibration, and uncertainty analysis, helping to evaluate and improve model performance.
+
+References: Taghikhah et al. (2022) https://doi.org/10.1016/j.ecolmodel.2022.109930
+
+## Installation
+
+TODO
+
+### Installing tools on Mac
+
+TODO
+
+## Getting started
+
+Make your conda environment called `daesim-analysis` using the environment.yml file and the command:
+
+$ conda env create --name daesim-analysis --file environment.yml
+
+Then, you can activate your new environment with the command:
+
+$ conda activate daesim-analysis
+
+Now you have created the conda virtual environment to work in, we must install the package. To install the package use the command:
+
+$ pip install -e .
+
+Hopefully everything installed successfully. 
+
+You can now run the code or open a jupyter notebook to start testing. To open a jupyter notebook run `jupyter-notebook` to open a Jupyter Notebook server or run `jupyter-lab` to open a JupyterLab server (recommended). From there, go into the notebooks directory and work through the examples.
+
+Once you're finished, you can deactivate the conda environment with the command:
+
+$ conda deactivate
+
+### Why do we use Anaconda?
+
+There are benefits to using Anaconda rather than just using pip. As discussed in this article (https://pythonspeed.com/articles/conda-vs-pip/) the main benefits are portability and reproducibility.
+- Portability across operating systems: Instead of installing Python in three different ways on Linux, macOS, and Windows, you can use the same environment.yml on all three.
+- Reproducibility: It’s possible to pin almost the whole stack, from the Python interpreter upwards.
+- Consistent configuration: You don’t need to install system packages and Python packages in two different ways; (almost) everything can go in one file, the environment.yml.
+
+Using Conda also addresses another problem: How to deal with Python libraries that require compiled code? At some point we may like to convert some of the DAESim source code into another, more computationally efficient language (e.g. C++ or Fortran) so that we can run larger simulations. If we decide to do that we will need to compile the source code in a language other than Python, in which case pip would be insufficient and Conda will be required. 
+
+### Linking to the DAESIM2 github repository
+
+This toolkit of helper functions and examples is kept independent from the source code of the DAESIM2 biophysical model. This keeps things tidier and prevents possible ballooning of the code base size that must be maintained. It is useful to keep the biophysical model independent from these tools as we want users to be able to go in and run the model without having to understand or get lost in the rest of the analysis support tools e.g. calibration, sensitivity analysis, uncertainty analysis. In addition, the analysis tools here require specific python libraries that we don't want the biophysical model code base to have dependencies on. 
+
+### Why do I only see `.py` files in the `notebooks` directory?
+
+Tracking notebooks is a pain because of all the extra metadata that is in them.
+As a result, we use [jupytext](https://jupytext.readthedocs.io/).
+This allows us to track clean `.py` files while keeping all the power of jupyter notebooks.
+You'll notice that we ignore `*.ipynb` in our `.gitignore`.
+
+Thanks to jupytext, you can just click on the `.py` files and they will open like standard notebooks.
+
+## General guidance on working in this repository
+
+### Repository guiding principles
+
+Text todo: Outline how to use this repository. e.g. /data/ should only contain a test set of forcing data, do not commit large datasets to it. Similarly, do not commit anything to /results/. Keep the repo clean and tidy. 
+
+### Branch etiquette
+
+In general, don't push to other people's branches (things can get weird if two people work on the same branch at once).
+Instead, check out the branch and then create your own branch in case you want to make any commits (see "Checking out a branch related to a merge request" below).
+Then, to add your changes, make a merge request from your own branch back into the original branch.
+
+## Support
+
+If you need support, the first place to go is the [issue tracker] (todo: link the repository / issues).
+From there, you can tag other model users to ask for help.
+As a second step, reach out directly to your collaborators.
+
+## Other helpful snippets
+
+### Checking out a branch related to a merge request
+
+```sh
+# Fetch any changes on the remote server
+git fetch origin
+# If you get no output, you're up to date
+# If you get output, it will show you which branches have changed
+# on the remote server
+
+# Checkout the others' branch and create a local branch to track it
+# git checkout -b branch-name origin/branch-name
+# for example:
+git checkout -b local-source origin/local-source
+
+# Activate your environment
+conda activate daesim-analysis
+
+[remove]# Call make (just in case, you don't always have to do this)
+# make conda-environment
+
+# If any dependencies have changed, update your conda environment
+conda env update --file environment.yml --prune
+
+# Checkout (create) your own branch in case you want to make any commits
+# (typically we use the branch name plus our initials)
+git checkout -b test-notebook-an
+
+# You're now ready to work
+# E.g. by starting a notebook server and looking at the notebooks
+jupyter notebook
+```
+
+## Reading and viewing
+
+Not all of these will be neccesary for everyone, but a helpful list
+
+### What is self
+
+https://www.educative.io/answers/what-is-self-in-python
+
+### General approach to coding
+
+- [Clean Code](https://thixalongmy.haugiang.gov.vn/media/1175/clean_code.pdf) (buying the book is also a good option)
+- [Refactoring](http://silab.fon.bg.ac.rs/wp-content/uploads/2016/10/Refactoring-Improving-the-Design-of-Existing-Code-Addison-Wesley-Professional-1999.pdf) (buying the book is also a good option)
+- [Refactoring guru](refactoring.guru), incredible resource for understanding coding patterns and how to make them better (There is also a book, could be worth investing in)
+- [End of object inheritance](https://www.youtube.com/watch?v=3MNVP9-hglc) This one is hard to explain and understand until you start writing lots of code, but its worth watching (and re-watching) to understand the coding style you see in new climate models (e.g. MAGICC)
+- [End of object inheritance](https://www.youtube.com/watch?v=3MNVP9-hglc) This one is hard to explain and understand until you start writing lots of code, but its worth watching (and re-watching) to understand the coding style you see in new climate models (e.g. MAGICC)
+- [Composition over inheritance](https://www.youtube.com/watch?v=0mcP8ZpUR38) A nice explainer to see the principles of the above in practice
+- [Dependency injection vs. inversion](https://www.youtube.com/watch?v=2ejbLVkCndI) A further explainer to see the above in practice
+
+### Scientific software
+
+- [Research software engineering with Python](https://merely-useful.tech/py-rse/) A little bit out of date, but a good resource for general practice and examples of developing software with python
+
+### Numerical coding
+
+- [Numerical recipes](http://numerical.recipes/book/book.html) (buying the book is also a good option)
+
+### Big climate model output and handling netCDF files
+
+- [Software Carpentry's introduction to Python for atmosphere and ocean scientists](https://carpentries-lab.github.io/python-aos-lesson/), particularly if you think you're going to be working with netCDF files
+- [CMIP6 in the cloud](https://medium.com/pangeo/cmip6-in-the-cloud-five-ways-96b177abe396), particularly if you're going to be dealing with CMIP data a lot (although speak with someone about how far you need to go, it's unlikely that every step will be relevant)
+
+### Miscellaneous
+
+- [Basic introduction to Jupyter notebooks](https://realpython.com/jupyter-notebook-introduction/)
+
+## Recommended tools
+
+- An IDE (e.g. pycharm) or more light-weight editor (e.g. Sublime)
+- If on Mac, homebrew
+- Git
+- Make
+- Slack
+- gfortran
+- cmake
---
+++ src/analysis/fast_sensitivity.py
@@ -0,0 +1,345 @@
+"""
+Analysis helper functions to support DAESIM2 analysis, sensitivity, calibration, etc.
+"""
+
+from typing import Any
+from netCDF4 import date2num
+from datetime import datetime, timedelta
+import time
+import subprocess
+
+def run_model_and_get_outputs(Plant, ODEModelSolver, time_axis, forcing_inputs, reset_days, zero_crossing_indices):
+    ## Define the callable calculator that defines the right-hand-side ODE function
+    PlantCalc = Plant.calculate
+    
+    Model = ODEModelSolver(calculator=PlantCalc, states_init=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], time_start=time_axis[0], log_diagnostics=True)
+
+    Model.reset_diagnostics()
+    
+    ## Run the model solver
+    res = Model.run(
+        time_axis=time_axis,
+        forcing_inputs=forcing_inputs,
+        solver="euler",
+        zero_crossing_indices=zero_crossing_indices,
+        reset_days=reset_days,
+    )
+
+    # Convert the defaultdict to a regular dictionary
+    _diagnostics = dict(Model.diagnostics)
+    # Convert each list in the dictionary to a NumPy array
+    diagnostics = {key: np.array(value) for key, value in _diagnostics.items()}
+
+    # Convert the array to a numeric type, handling mixed int and float types
+    diagnostics['idevphase_numeric'] = np.array(diagnostics['idevphase'],dtype=np.float64)
+    
+    # In the model idevphase can equal None but that is not useable in post-processing, so we set None values to np.nan
+    diagnostics["idevphase_numeric"][diagnostics["idevphase"] == None] = np.nan
+
+    ## Conversion notes: When _E units are mol m-2 s-1, multiply by molar mass H2O to get g m-2 s-1, divide by 1000 to get kg m-2 s-1, multiply by 60*60*24 to get kg m-2 d-1, and 1 kg m-2 d-1 = 1 mm d-1. 
+    ## Noting that 1 kg of water is equivalent to 1 liter (L) of water (because the density of water is 1000 kg/m³), and 1 liter of water spread over 1 square meter results in a depth of 1 mm
+    diagnostics["E_mmd"] = diagnostics["E"]*18.015/1000*(60*60*24)
+    
+    # Turnover rates per pool
+    _tr_Leaf = np.zeros(diagnostics['t'].size)
+    _tr_Root = np.zeros(diagnostics['t'].size)
+    _tr_Stem = np.zeros(diagnostics['t'].size)
+    _tr_Seed = np.zeros(diagnostics['t'].size)
+    for it, t in enumerate(diagnostics['t']):
+        if np.isnan(diagnostics['idevphase_numeric'][it]):
+            tr_ = PlantX.PlantDev.turnover_rates[-1]
+            _tr_Leaf[it] = tr_[PlantX.PlantDev.ileaf]
+            _tr_Root[it] = tr_[PlantX.PlantDev.iroot]
+            _tr_Stem[it] = tr_[PlantX.PlantDev.istem]
+            _tr_Seed[it] = tr_[PlantX.PlantDev.iseed]
+        else:
+            tr_ = PlantX.PlantDev.turnover_rates[diagnostics['idevphase'][it]]
+            _tr_Leaf[it] = tr_[PlantX.PlantDev.ileaf]
+            _tr_Root[it] = tr_[PlantX.PlantDev.iroot]
+            _tr_Stem[it] = tr_[PlantX.PlantDev.istem]
+            _tr_Seed[it] = tr_[PlantX.PlantDev.iseed]
+    
+    diagnostics['tr_Leaf'] = _tr_Leaf
+    diagnostics['tr_Root'] = _tr_Root
+    diagnostics['tr_Stem'] = _tr_Stem
+    diagnostics['tr_Seed'] = _tr_Seed
+
+    # Add np.nan to the end of each array in the dictionary to represent the last time point in the time_axis (corresponds to the last time point of the state vector)
+    for key in diagnostics:
+        if key == "t":
+            diagnostics[key] = np.append(diagnostics[key], res["t"][-1])
+        else:
+            diagnostics[key] = np.append(diagnostics[key], np.nan)
+
+    
+    # Add state variables to the diagnostics dictionary
+    diagnostics["Cleaf"] = res["y"][0,:]
+    diagnostics["Cstem"] = res["y"][1,:]
+    diagnostics["Croot"] = res["y"][2,:]
+    diagnostics["Cseed"] = res["y"][3,:]
+    diagnostics["Bio_time"] = res["y"][4,:]
+    diagnostics["VRN_time"] = res["y"][5,:]
+    diagnostics["Cstate"] = res["y"][7,:]
+    diagnostics["Cseedbed"] = res["y"][8,:]
+
+    # Add forcing inputs to diagnostics dictionary
+    for i,f in enumerate(forcing_inputs):
+        ni = i+1
+        if f(time_axis[0]).size == 1:
+            fstr = f"forcing {ni:02}"
+            diagnostics[fstr] = f(time_axis)
+        elif f(time_axis[0]).size > 1:
+            # this forcing input has levels/layers (e.g. multilayer soil moisture)
+            nz = f(time_axis[0]).size
+            for iz in range(nz):
+                fstr = f"forcing {ni:02} z{iz}"
+                diagnostics[fstr] = f(time_axis)[:,iz]
+
+    total_carbon_t = res["y"][PlantX.PlantDev.ileaf,:] + res["y"][PlantX.PlantDev.istem,:] + res["y"][PlantX.PlantDev.iroot,:] + res["y"][PlantX.PlantDev.iseed,:]
+    total_carbon_exclseed_t = res["y"][PlantX.PlantDev.ileaf,:] + res["y"][PlantX.PlantDev.istem,:] + res["y"][PlantX.PlantDev.iroot,:]
+    
+    it_peakbiomass = np.argmax(total_carbon_t)
+    it_peakbiomass_exclseed = np.argmax(total_carbon_exclseed_t)
+    
+    it_sowing = np.where(time_axis == PlantX.Management.sowingDay)[0][0]
+    if PlantX.Management.harvestDay is not None:
+        it_harvest = np.where(time_axis == PlantX.Management.harvestDay)[0][0]
+    else:
+        it_harvest = -1   # if there is no harvest day specified, we just take the last day of the simulation. 
+    
+    # Diagnose time indexes when developmental phase transitions occur
+    
+    # Convert the array to a numeric type, handling mixed int and float types
+    idevphase = diagnostics["idevphase_numeric"]
+    valid_mask = ~np.isnan(idevphase)
+    
+    # Identify all transitions (number-to-NaN, NaN-to-number, or number-to-different-number)
+    it_phase_transitions = np.where(
+        ~valid_mask[:-1] & valid_mask[1:] |  # NaN-to-number
+        valid_mask[:-1] & ~valid_mask[1:] |  # Number-to-NaN
+        (valid_mask[:-1] & valid_mask[1:] & (np.diff(idevphase) != 0))  # Number-to-different-number
+    )[0] + 1
+    
+    # Time index for the end of the maturity phase
+    if PlantX.PlantDev.phases.index('maturity') in idevphase:
+        it_mature = np.where(idevphase == PlantX.PlantDev.phases.index('maturity'))[0][-1]    # Index for end of maturity phase
+    elif PlantX.Management.harvestDay is not None: 
+        it_mature = it_harvest    # Maturity developmental phase not completed, so take harvest as the end of growing season
+    else:
+        it_mature = -1    # if there is no harvest day specified, we just take the last day of the simulation. 
+
+    # Filter out transitions that occur after the maturity or harvest day
+    it_phase_transitions = [t for t in it_phase_transitions if time_axis[t] <= time_axis[it_mature]]
+
+    # Developmental phase indexes
+    igermination = PlantX.PlantDev.phases.index("germination")
+    ivegetative = PlantX.PlantDev.phases.index("vegetative")
+    if PlantX.Management.cropType == "Wheat":
+        ispike = PlantX.PlantDev.phases.index("spike")
+    ianthesis = PlantX.PlantDev.phases.index("anthesis")
+    igrainfill = PlantX.PlantDev.phases.index("grainfill")
+    imaturity = PlantX.PlantDev.phases.index("maturity")
+
+    # Carbon and Water
+    W_P_peakW = total_carbon_t[it_peakbiomass]/PlantX.PlantCH2O.f_C    # Total dry biomass at peak biomass
+    W_L_peakW = res["y"][PlantX.PlantDev.ileaf,it_peakbiomass]/PlantX.PlantCH2O.f_C    # Leaf dry biomass at peak biomass
+    W_R_peakW = res["y"][PlantX.PlantDev.istem,it_peakbiomass]/PlantX.PlantCH2O.f_C    # Root dry biomass at peak biomass
+    W_S_peakW = res["y"][PlantX.PlantDev.iroot,it_peakbiomass]/PlantX.PlantCH2O.f_C    # Stem dry biomass at peak biomass
+    if PlantX.Management.cropType == "Wheat":
+        ip = np.where(diagnostics['idevphase'][it_phase_transitions] == PlantX.PlantDev.phases.index('spike'))[0][0]
+        W_S_spike0 = res["y"][PlantX.PlantDev.istem,it_phase_transitions[ip]]/PlantX.PlantCH2O.f_C    # Stem dry biomass at start of spike
+    ip = np.where(diagnostics['idevphase'][it_phase_transitions] == PlantX.PlantDev.phases.index('anthesis'))[0][0]
+    W_S_anth0 = res["y"][PlantX.PlantDev.istem,it_phase_transitions[ip]]/PlantX.PlantCH2O.f_C    # Stem dry biomass at start of anthesis
+    GPP_int_seas = np.sum(diagnostics['GPP'][it_sowing:it_mature+1])    # Total (integrated) seasonal GPP
+    NPP_int_seas = np.sum(diagnostics['NPP'][it_sowing:it_mature+1])    # Total (integrated) seasonal NPP
+    Rml_int_seas = np.sum(diagnostics['Rml'][it_sowing:it_mature+1])    # Total (integrated) seasonal Rml
+    Rmr_int_seas = np.sum(diagnostics['Rmr'][it_sowing:it_mature+1])    # Total (integrated) seasonal Rmr
+    Rg_int_seas = np.sum(diagnostics['Rg'][it_sowing:it_mature+1])    # Total (integrated) seasonal Rg
+    trflux_int_seas = np.sum(diagnostics['trflux_total'][it_sowing:it_mature+1])    # Total (integrated) seasonal turnover losses
+    FCstem2grain_int_seas = np.sum(diagnostics['F_C_stem2grain'][it_sowing:it_mature+1])    # Total (integrated) remobilisation to grain
+    _Cflux_NPP2grain = diagnostics['u_Seed'] * diagnostics['NPP']    # NPP carbon allocation flux to grain
+    NPP2grain_int_seas = np.sum(_Cflux_NPP2grain[it_sowing:it_mature+1])    # Total (integrated) NPP carbon allocation to grain
+    E_int_seas = np.sum(diagnostics['E_mmd'][it_sowing:it_mature+1])    # Total (integrated) seasonal transpiration
+    LAI_peakW = diagnostics['LAI'][it_peakbiomass]    # Leaf area index at peak biomass
+    
+    # Grain Production
+    W_spike_anth1 = res["y"][7,it_mature]/PlantX.PlantCH2O.f_C    # Spike dry biomass at anthesis
+    GY_mature = res["y"][PlantX.PlantDev.iseed,it_mature]/PlantX.PlantCH2O.f_C    # Grain yield at maturity
+    GY_harvest = res["y"][PlantX.PlantDev.iseed,it_harvest]/PlantX.PlantCH2O.f_C    # Grain yield at harvest
+    Sdpot_mature = diagnostics['S_d_pot'][it_mature]    # Potential seed density (grain number density) at maturity
+    Sdpot_harvest = diagnostics['S_d_pot'][it_harvest]    # Potential seed density (grain number density) at harvest
+    if PlantX.Management.cropType == "Wheat":
+        GN_mature = res["y"][PlantX.PlantDev.iseed,it_mature]/PlantX.PlantCH2O.f_C/PlantX.W_seedTKW0    # Actual grain number at maturity
+        GN_harvest = res["y"][PlantX.PlantDev.iseed,it_harvest]/PlantX.PlantCH2O.f_C/PlantX.W_seedTKW0    # Actual grain number at harvest
+    
+    # Model output (of observables) given the parameter vector p
+    # - this is the model output that we compare to observations and use to calibrate the parameters
+    M_p = np.array([
+        W_P_peakW, 
+        W_L_peakW,
+        W_R_peakW,
+        W_S_peakW,
+        W_S_spike0,
+        W_S_anth0,
+        GPP_int_seas,
+        NPP_int_seas,
+        Rml_int_seas,
+        Rmr_int_seas,
+        Rg_int_seas,
+        trflux_int_seas,
+        FCstem2grain_int_seas,
+        NPP2grain_int_seas,
+        E_int_seas,
+        LAI_peakW,
+        W_spike_anth1,
+        GY_mature,
+        Sdpot_mature,
+        GN_mature,
+    ])
+
+    return M_p, diagnostics
+
+def update_attribute(obj: Any, path: str, new_value: Any) -> None:
+    """
+    Update the attribute specified by the path with a new value.
+
+    Parameters:
+    obj (Any): The main class instance to update.
+    path (str): Dot-separated path to the attribute (e.g., "Site.temperature").
+    new_value (Any): The new value to set for the attribute.
+    """
+    attributes = path.split('.')
+    # Traverse the path except for the last attribute
+    for attr in attributes[:-1]:
+        obj = getattr(obj, attr)
+
+    # Set the new value to the last attribute in the path
+    setattr(obj, attributes[-1], new_value)
+
+def update_attribute_in_phase(obj: Any, path: str, new_value: Any, phase: str) -> None:
+    """
+    Update the attribute specified by the path and a specific developmental phase with a new value.
+
+    Parameters:
+    obj (Any): The main class instance to update.
+    path (str): Dot-separated path to the attribute (e.g., "Site.temperature").
+    new_value (Any): The new value to set for the attribute.
+    phase (str): The plant developmental phase of the attribute
+    """
+    attributes = path.split('.')
+    # Traverse the path except for the last attribute
+    for attr in attributes[:-1]:
+        obj = getattr(obj, attr)
+    # Make a copy of the phase-specific values
+    new_phase_values = getattr(obj, attributes[-1])
+    # Get the phase index for the value, assuming the obj is the correct module that includes the "phases" attribute
+    phase_i = obj.phases.index(phase)
+    # Update only the phase-specific value
+    new_phase_values[phase_i] = new_value
+    # Set the new phase value list to the last attribute in the path
+    setattr(obj, attributes[-1], new_phase_values)
+
+def model_function(params, Plant, input_data, params_info, problem):
+    """
+    Function to update the model parameters, run the model and return the outputs
+
+    Parameters
+    ----------
+    params : array_like
+        Vector of parameter values
+    Plant : Callable
+        Model class that is updated and run
+    input_data : list
+        List of input data for running the model (e.g. forcing_inputs, ODEModelSolver inputs)
+    params_info : pd.DataFrame()
+        Pandas dataframe with parameter information including parameter name ("Name") and model class path ("Module Path")
+    problem : dict
+        Dictionary required as input to SALib FAST sampler (includes keys: "num_vars", "names", "bounds")
+
+    Returns
+    -------
+    array_like
+        Output from model run
+    """
+    # Update the model class with the new parameters
+    p_x = params
+    for j, p_xi in enumerate(p_x):
+        # print("Parameter:",problem["names"][j])
+        # print("Module path:",parameters_df.loc[parameters_df["Name"] == problem["names"][j]]["Module Path"].values)
+        # print("Full module path:",parameters_df.loc[parameters_df["Name"] == problem["names"][j]]["Module Path"].values + "." + str(problem["names"][j])
+        # print("Value:",p_xi)
+        path = params_info.loc[params_info["Name"] == problem["names"][j]]["Module Path"].values[0] + "." + str(problem["names"][j])
+        if params_info.loc[params_info["Name"] == problem["names"][j]]["Phase Specific"].values[0] == True:
+            # if the parameter is specific to a phase in PlantDev then we handle it differently, as the attribute is a list of values corresponding to each phase but we only want to update one value in that list
+            phase = params_info.loc[params_info["Name"] == problem["names"][j]]["Phase"].values[0]
+            update_attribute_in_phase(Plant, path, p_xi, phase)
+        else:
+            update_attribute(Plant, path, p_xi)
+
+    # Collate input data to pass to model run function
+    ODEModelSolver, time_axis, forcing_inputs, reset_days, zero_crossing_indices = input_data
+    # Make sure the solver knows about the sowing and harvest dates as well (to reset the state variables like GDD and VD)
+    # reset_days = [Plant.Management.sowingDay, Plant.Management.harvestDay]
+    
+    model_output = run_model_and_get_outputs(Plant, ODEModelSolver, time_axis, forcing_inputs, reset_days, zero_crossing_indices)
+
+    return model_output
+
+
+def write_diagnostics_to_nc(Model, model_diagnostics, filepath, filename, time_axis, time_nday, time_year, problem, param_values):
+    # Create datetimes for writing the netcdf
+    # years and days-of-year
+    tinds = np.where((time_nday >= time_axis[0]) & (time_nday <= time_axis[-1]))
+    years = list(np.array(time_year)[tinds])
+    days_of_year = list(np.array(time_doy)[tinds])
+    
+    # Convert to datetime objects
+    dates = [datetime(year, 1, 1) + timedelta(days=day - 1) for year, day in zip(years, days_of_year)]
+    
+    # Define a reference date (e.g., 1900-01-01)
+    reference_date = datetime(1900, 1, 1)
+    
+    # Calculate days since the reference date
+    time_data = np.array([(date - reference_date).days for date in dates])
+     
+    def get_git_hash():
+        try:
+            # Get the current commit hash
+            git_hash = subprocess.check_output(["git", "rev-parse", "HEAD"]).strip().decode("utf-8")
+            return git_hash
+        except subprocess.CalledProcessError:
+            return "Unknown"
+    
+    # Open a new NetCDF file
+    with Dataset(filepath+filename, "w", format="NETCDF4") as ncfile:
+        # Define dimensions
+        time_dim = ncfile.createDimension("time", None)  # None for unlimited (appendable) time dimension
+        
+        # Create the time variable
+        time_var = ncfile.createVariable("time", "i8", ("time",))
+        time_var.units = "days since 1900-01-01"
+        time_var.calendar = "standard"  # Common calendar format; adjust if needed
+        
+        # Write the time data to the file
+        time_var[:] = time_data
+        
+        # Write model outputs stored in a dictionary
+        for var_name, data in model_diagnostics.items():
+            if var_name == "t":
+                continue
+            else:
+                var = ncfile.createVariable(var_name, "f4", ("time",))
+                var[:] = data
+                # var.units = "example units"  # Add units for each variable
+                # var.long_name = f"Description of {var_name}"  # Metadata for clarity
+        
+        # Example of adding class attribute metadata as a global attribute
+        ncfile.title = "DAESIM2-Plant Model FAST Sensitivty Analysis"
+        ncfile.description = "DAESIM2-Plant model FAST sensitivity analysis"  #for the site %s and simulation number %d" % (xsite,nparamset)
+        ncfile.DAESIMcalculator = str(Model)
+        ncfile.history = "Created on " + time.ctime(time.time())
+        ncfile.institution = "Research School of Biology, Australian National University"
+        ncfile.source = "Generated by DAESIM2-Plant Model v1.0 (GitHub Repository: https://github.com/NortonAlex/DAESIM)"
+        ncfile.FASTproblem = str(problem).replace("\n","")
+        ncfile.FASTpars = ", ".join(f"{name}={value}" for name, value in zip(problem['names'], param_values))
+        ncfile.git_hash = get_git_hash()
